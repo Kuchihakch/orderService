01fce88ddabedfe50761c7fd59d12317f26e4df3
FelienZ <rajabfadh@gmail.com>
2026-01-24 16:16:51 +0700

    feat(main): integrate all services and repositories

--- main.go
+++ main.go
@@ -8,19 +8,69 @@ import (
 )
 
 func main() {
-	repo := repo.NewRepository()
-	services := services.OrderServiceImpl{Repo: repo}
+	productRepo := repo.NewProductRepository()
+	productService := services.ProductServiceImpl{ProductRepo: productRepo}
 
-	order1 := models.Order{
-		ID:     "order-1",
-		Amount: 3,
-		UserID: "user-1",
+	orderRepo := repo.NewRepository()
+	orderService := services.OrderServiceImpl{OrderRepo: orderRepo, ProductRepo: productRepo}
+
+	// bikin product dulu (as Admin)
+	product1 := models.Product{
+		ID:    "product-1",
+		Name:  "komik my kisah",
+		Stock: 3,
 	}
-	services.CreateOrder(order1)
-	fmt.Println(services.FindAll())
-	fmt.Println(services.FindByUserID("user-1"))
-	fmt.Println(services.FindById("order-2")) // notfound
-	services.DeleteOrder("order-1")
-	fmt.Println(services.FindAll())
-	fmt.Println(services.FindById("order-1"))
+	product2 := models.Product{
+		ID:    "product-2",
+		Name:  "tips n trik kripto",
+		Stock: 1,
+	}
+	productService.CreateProduct(product1)
+	productService.CreateProduct(product2)
+
+	fmt.Println(productService.GetProductByID("product-1"))
+
+	//order as User
+	order1 := models.Order{
+		ID:        "order-1",
+		ProductID: "product-1",
+		UserID:    "user-1",
+		Amount:    2,
+	}
+	// melebihi stock product 2
+	order2 := models.Order{
+		ID:        "order-2",
+		ProductID: "product-2",
+		UserID:    "user-2",
+		Amount:    2,
+	}
+	// aman race, update stock ketika pay
+	order3 := models.Order{
+		ID:        "order-3",
+		ProductID: "product-1",
+		UserID:    "user-2",
+		Amount:    2,
+	}
+	// notfound product
+	order4 := models.Order{
+		ID:        "order-4",
+		ProductID: "product-5",
+		UserID:    "user-1",
+		Amount:    2,
+	}
+
+	orderService.CreateOrder(order1)
+	orderService.CreateOrder(order2)
+	orderService.CreateOrder(order3)
+	orderService.CreateOrder(order4)
+
+	fmt.Println(orderService.FindAll())
+
+	fmt.Println(orderService.PayOrder("order-1"))
+	//cek race order yang sama
+	fmt.Println(orderService.PayOrder("order-3"))
+
+	//admin update dulu stok
+	productService.UpdateProductStock("product-1", 5)
+	fmt.Println(orderService.PayOrder("order-3"))
 }
